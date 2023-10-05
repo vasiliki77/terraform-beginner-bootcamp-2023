a4e344db3433116a0df8f53fa2b70a7df0267a4b
vasiliki77 <145263320+vasiliki77@users.noreply.github.com>
2023-10-05 20:05:00 +0100

    #47 test our terra home on terratowns test town (#48)

--- variables.tf
+++ variables.tf
@@ -1,5 +1,13 @@
-variable "user_uuid" {
+variable "teacherseat_user_uuid" {
     type = string
+}
+
+variable "terratowns_access_token" {
+    type = string
+}
+
+variable "terratowns_endpoint" {
+  type = string
 }
 
 variable "bucket_name" {
