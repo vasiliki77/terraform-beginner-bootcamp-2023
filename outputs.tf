7003da764d88bd62368521ed356fe372e699fa2b
vasiliki77 <145263320+vasiliki77@users.noreply.github.com>
2023-10-05 12:34:03 +0100

    43 terratowns provider (#45)
    * #43 wip setting up the skeleton for creating a custom terraform provider
    
    * #43 implement terraform provider block in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "The cloudfront distribution domain name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The cloudfront distribution domain name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
