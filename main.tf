a19dcc38acb4ae517b1989af43022ba2658fb742
vasiliki77 <145263320+vasiliki77@users.noreply.github.com>
2023-10-05 19:01:47 +0100

    43 terratowns provider (#46)
    * #43 wip setting up the skeleton for creating a custom terraform provider
    
    * #43 implement terraform provider block in terraform
    
    * #43 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint   = "http://localhost:4567/api"
+  user_uuid  = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+  token      = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 # module "terrahouse_aws" {
 #   source = "./modules/terrahouse_aws"
@@ -27,3 +27,15 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "Living underwater!!"
+  description = <<DESCRIPTION
+Bikini Bottom is a real place.
+It's a fresh water bubble on the ocean floor.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
