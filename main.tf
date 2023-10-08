e5ae7515f1d7c400d0d21f873246d8f7e73111e3
vasiliki77 <145263320+vasiliki77@users.noreply.github.com>
2023-10-08 16:36:32 +0100

    #49 fix issues with provider, set up two different towns (#50)

--- main.tf
+++ main.tf
@@ -5,36 +5,51 @@ terraform {
       version = "1.0.0"
     }
   }
-#cloud {
-#  organization = "example-org-da89f8"
-#
-#  workspaces {
-#    name = "terra-house-1"
-#  }
-}
+    cloud {
+    organization = "example-org-da89f8"
 
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+}
 provider "terratowns" {
   endpoint   = var.terratowns_endpoint
   user_uuid  = var.teacherseat_user_uuid
   token      = var.terratowns_access_token
 }
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_spongebob_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  bucket_name = var.bucket_name
-  error_html_filepath = var.error_html_filepath
-  index_html_filepath = var.index_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.spongebob.public_path
+  content_version = var.spongebob.content_version
 }
 
-resource "terratowns_home" "home" {
+resource "terratowns_home" "home_spondebob" {
   name = "Living underwater!!"
   description = <<DESCRIPTION
 Bikini Bottom is a real place.
 It's a fresh water bubble on the ocean floor.
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_spongebob_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.spongebob.content_version
+}
+
+module "home_lacta_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.lacta.public_path
+  content_version = var.lacta.content_version
+}
+
+resource "terratowns_home" "home_lacta" {
+  name = "Lacta lover!!"
+  description = <<DESCRIPTION
+Lacta is the best chocolate in the world!
+It's the best valentines gift.
+DESCRIPTION
+  domain_name = module.home_lacta_hosting.domain_name
+  town = "missingo"
+  content_version = var.lacta.content_version
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
+  value = module.home_spongebob_hosting.bucket_name
 }
 
 output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
+  description = "S3 Static Website hosting endpoint"
+  value = module.home_spongebob_hosting.website_endpoint
 }
 
 output "cloudfront_url" {
-  description = "The cloudfront distribution domain name"
-  value = module.terrahouse_aws.cloudfront_url
+  description = "The CloudFront Distribution Domain Name"
+  value = module.home_spongebob_hosting.domain_name
 }
